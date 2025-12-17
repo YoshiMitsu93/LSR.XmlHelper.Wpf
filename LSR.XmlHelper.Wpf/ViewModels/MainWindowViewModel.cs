366c2cfa4b66e3f2ba91ea83145f35cd29d5a9e1
Y0sh1M1tsu <mickielee2015@hotmail.com>
2025-12-17 00:52:56 +0000

    Testing shit

--- LSR.XmlHelper.Wpf/ViewModels/MainWindowViewModel.cs
+++ LSR.XmlHelper.Wpf/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,18 @@
-﻿namespace LSR.XmlHelper.Wpf.ViewModels
+﻿using System.Windows;
+using System.Windows.Input;
+using LSR.XmlHelper.Wpf.Infrastructure;
+
+namespace LSR.XmlHelper.Wpf.ViewModels
 {
     public sealed class MainWindowViewModel
     {
         public string Title => "LSR XML Helper";
+
+        public ICommand TestCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            TestCommand = new RelayCommand(() => MessageBox.Show("RelayCommand works!"));
+        }
     }
 }
